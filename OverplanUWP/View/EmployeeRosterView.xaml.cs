d600c94116b6bee8338cba4831e9193ee424df41
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-12-02 10:30:36 +0100

    Lavede NavigationView for når man logger ind som enten employee eller leader

--- OverplanUWP/View/EmployeeRosterView.xaml.cs
+++ OverplanUWP/View/EmployeeRosterView.xaml.cs
@@ -26,5 +26,6 @@ namespace OverplanUWP.View
         {
             this.InitializeComponent();
         }
+        
     }
 }
--- OverplanUWP/View/LeaderShiftsView.xaml.cs
+++ OverplanUWP/View/LeaderShiftsView.xaml.cs
@@ -20,20 +20,11 @@ namespace OverplanUWP.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class LoginView : Page
+    public sealed partial class LeaderShiftsView : Page
     {
-        public LoginView()
+        public LeaderShiftsView()
         {
             this.InitializeComponent();
-        }
-
-        private void click_employeeRosterView(object sender, RoutedEventArgs e)
-        {
-            LoginFrame.Navigate(typeof(EmployeeHomeView));
-        }
-        private void click_leaderRosterView(object sender, RoutedEventArgs e)
-        {
-            LoginFrame.Navigate(typeof(LeaderHomeView));
         }
     }
 }
