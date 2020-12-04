01585576ec25bcfb7344191be13eb8b2e76e2d65
KristianSpaqaj <kris49j7@edu.easj.dk>
2020-12-04 18:30:22 +0100

    Omdøbede variabler

--- OverplanUWP/ViewModel/TestViewModel.cs
+++ OverplanUWP/ViewModel/TestViewModel.cs
@@ -12,84 +12,79 @@ using System.Threading.Tasks;
 
 namespace OverplanUWP.ViewModel
 {
-    
+
     public class TestViewModel
     {
-        public ObservableCollection<ShiftOverview> getShiftoverview { get; set; }
-        public ObservableCollection<EmployeeOverview> getMedarbejdersplan { get; set; }
-        public EmployeeOverview selectedEmployee { get; set; }
+        public ObservableCollection<ShiftOverview> ShiftOverviews { get; set; }
+        public ObservableCollection<EmployeeOverview> EmployeeOverviews { get; set; }
+        public EmployeeOverview SelectedEmployee { get; set; }
 
-        public int shiftID { get; set; }
-        public int employeeID { get; set; }
-        public string name { get; set; }
-        public string adress { get; set; }
-        public string number { get; set; }
-        public DateTimeOffset dateFrom { get; set; } 
-        public DateTimeOffset dateTo { get; set; } 
-        public TimeSpan timeFrom { get; set; }
-        public TimeSpan timeTo { get; set; }
-
-        
-
-        public RelayCommand AddMedarbejder { get; set; }
-        public RelayCommand HentMedarbejder { get; set; }
-
-        public RelayCommand postShift { get; set; }
-        public RelayCommand getShift { get; set; }
+        public int ShiftID { get; set; }
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTimeOffset DateFrom { get; set; }
+        public DateTimeOffset DateTo { get; set; }
+        public TimeSpan TimeFrom { get; set; }
+        public TimeSpan TimeTo { get; set; }
+        public RelayCommand PostEmployeeOverviewCommand { get; set; }
+        public RelayCommand GetEmployeeOverviewCommand { get; set; }
+        public RelayCommand PostShiftOverviewCommand { get; set; }
+        public RelayCommand GetShiftOverviewCommand { get; set; }
 
         public TestViewModel()
         {
+            ShiftOverviews = new ObservableCollection<ShiftOverview>();
+            EmployeeOverviews = new ObservableCollection<EmployeeOverview>();
 
-            getShiftoverview = new ObservableCollection<ShiftOverview>();
-            getMedarbejdersplan = new ObservableCollection<EmployeeOverview>();
-
-            timeFrom = DateTime.Now.TimeOfDay;
-            timeTo = DateTime.Now.TimeOfDay.Add(new TimeSpan(8,0,0));
-            dateFrom = DateTime.Today;
-            dateTo = DateTime.Today + timeTo;
-
-            AddMedarbejder = new RelayCommand(PostEmployeeOverview);
-            HentMedarbejder = new RelayCommand(GetEmployeeOverview);
-            postShift = new RelayCommand(PostShiftOverview);
-            getShift = new RelayCommand(GetShiftOverview);
+            TimeFrom = DateTime.Now.TimeOfDay;
+            TimeTo = DateTime.Now.TimeOfDay;
+            DateFrom = DateTime.Today;
+            DateTo = CombineDateTime(DateFrom, TimeTo);
+            PostEmployeeOverviewCommand = new RelayCommand(PostEmployeeOverview);
+            GetEmployeeOverviewCommand = new RelayCommand(GetEmployeeOverview);
+            PostShiftOverviewCommand = new RelayCommand(PostShiftOverview);
+            GetShiftOverviewCommand = new RelayCommand(GetShiftOverview);
         }
         private async void PostEmployeeOverview()
         {
-            EmployeeOverview employee = new EmployeeOverview(employeeID, name, adress, number);
+            EmployeeOverview employee = new EmployeeOverview(EmployeeID, Name, Address, PhoneNumber);
             await Database.Post<EmployeeOverview>(employee);
         }
 
         private async void GetEmployeeOverview()
         {
-            getMedarbejdersplan.Clear();
+            EmployeeOverviews.Clear();
             var employees = Database.Get<EmployeeOverview>();
-            foreach(var e in await employees)
+            foreach (var e in await employees)
             {
-                getMedarbejdersplan.Add(e);
+                EmployeeOverviews.Add(e);
             }
         }
 
         private async void PostShiftOverview()
         {
-            DateTime from = CombineDateTime(dateFrom, timeFrom);
-            DateTime to = CombineDateTime(dateTo, timeTo);
-            ShiftOverview shift = new ShiftOverview(shiftID, selectedEmployee.EmployeeID, from, to);
+            DateTime from = CombineDateTime(DateFrom, TimeFrom);
+            DateTime to = CombineDateTime(DateTo, TimeTo);
+            ShiftOverview shift = new ShiftOverview(ShiftID, SelectedEmployee.EmployeeID, from, to);
             await Database.Post<ShiftOverview>(shift);
+        }
+
+
+        private async void GetShiftOverview()
+        {
+            ShiftOverviews.Clear();
+            var shifts = Database.Get<ShiftOverview>();
+            foreach (var e in await shifts)
+            {
+                ShiftOverviews.Add(e);
+            }
         }
 
         public DateTime CombineDateTime(DateTimeOffset dateTimeOffset, TimeSpan timeSpan)
         {
             return dateTimeOffset.Date + timeSpan;
-        }
-
-        private async void GetShiftOverview()
-        {
-            getShiftoverview.Clear();
-            var shifts = Database.Get<ShiftOverview>();
-            foreach (var e in await shifts)
-            {
-                getShiftoverview.Add(e);
-            }
         }
 
     }
